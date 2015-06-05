5014023ab7f3fb8e34489d2cef78b25138212cbf
Manu <bionikspoon@gmail.com>
2015-06-05 01:12:23 -0500

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularCmsApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
